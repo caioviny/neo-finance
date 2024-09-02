0292a993b13cb2db50943d739587a6bccbe96f09
Caio Vinícius <137078120+caioviny@users.noreply.github.com>
2024-09-01 22:16:48 -0300

    fix: adiconando versão 1.0

---
+++ assets/js/openPopUp.js
@@ -0,0 +1,16 @@
+     // Abrir o popup
+     document.getElementById('openPopupBtn').addEventListener('click', function () {
+        document.getElementById('popup').style.display = 'block';
+    });
+
+    // Fechar o popup
+    document.getElementById('closePopupBtn').addEventListener('click', function () {
+        document.getElementById('popup').style.display = 'none';
+    });
+
+    // Fechar o popup clicando fora do conteúdo
+    window.addEventListener('click', function (event) {
+        if (event.target == document.getElementById('popup')) {
+            document.getElementById('popup').style.display = 'none';
+        }
+    });
